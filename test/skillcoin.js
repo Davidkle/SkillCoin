92495832656f5aabc358cc4ac491b4a49cc357e5
David Le <davidkle@gmail.com>
2022-05-26 21:08:38 -0700

    Swap to js tests

---
+++ test/skillcoin.js
@@ -0,0 +1,21 @@
+const SkillCoin = artifacts.require("SkillCoin");
+
+contract('SkillCoin', (accounts) => {
+  const owner = accounts[0];
+  const user1 = accounts[1];
+  const user2 = accounts[2];
+  
+  it('should put 1B coins in the owner account', async () => {
+    const skillCoinInstance = await SkillCoin.deployed(owner);
+    const balance = await skillCoinInstance.balanceOf.call(owner);
+    assert.equal(balance.valueOf(), 1000000000);
+  });
+
+  it('should send coin correctly', async () => {
+    const skillCoinInstance = await SkillCoin.deployed(owner);
+    await skillCoinInstance.transfer(user1, 10);
+
+    const balanceUser1 = (await skillCoinInstance.balanceOf.call(user1)).toNumber();
+    assert.equal(balanceUser1, 10); 
+  });
+});
